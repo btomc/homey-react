44cb37b6f083ad6d8573600363bb4ab2b1c2dc90
Beata Tomczak <tma.btty@gmail.com>
2023-09-24 21:25:37 +0200

    Pages & routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
+import Home from './pages/Home'
+
 function App() {
-  return <div className='text-primary-100 text-lg'>Hello Homey</div>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<Login />} />
+          <Route path='/sign-up' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
+    </>
+  )
 }
 
 export default App
